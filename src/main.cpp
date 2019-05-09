a785bdf43ff2e8bbf10e9decddf067722aa9a47a
tommyjanna <tommyjanna@outlook.com>
2019-05-08 20:56:09 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+	RenderWindow window(VideoMode(640, 480), "Hello SFML");
+
+	CircleShape shape(100.f);
+	shape.setFillColor(Color::Blue);
+
+	while(window.isOpen())
+	{
+		Event event;
+		
+		while(window.pollEvent(event))
+		{
+			if(event.type == Event::Closed)
+			{
+				window.close();
+			}
+		}
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+	
+	return 0;
+}
